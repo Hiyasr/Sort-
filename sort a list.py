c0335b823853865ea958cb19a063f6abfc4948a1
Hiya Singh <150180001+Hiyasr@users.noreply.github.com>
2024-07-09 13:30:55 +0530

    Create sort a list.py

---
+++ sort a list.py
@@ -0,0 +1,8 @@
+# Define a list of numbers
+numbers = [5, 2, 9, 1, 5, 6]
+
+# Sort the list in ascending order
+numbers.sort()
+
+# Print the sorted list
+print("Sorted numbers in ascending order:", numbers)
